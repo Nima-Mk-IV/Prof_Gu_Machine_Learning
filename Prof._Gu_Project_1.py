1c48171e8cdf1c5e8e9868cddb3c88cb2311ca85
Vikram Amin <aminvikram@gmail.com>
2018-03-02 19:49:30 -0800

    3/2/18
    Fixed clustering and added a planar fit

--- Prof._Gu_Project_1.py
+++ Prof._Gu_Project_1.py
@@ -10,6 +10,9 @@ import numpy as np
 import numpy.polynomial.polynomial as poly
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from sklearn.cluster import KMeans
+
+import scipy.linalg
 
 #####################
 ###hyperparameters###
@@ -17,12 +20,17 @@ from mpl_toolkits.mplot3d import Axes3D
 
 data_points_per_series=5
 degree_for_fitting=3
+n_clusters=3
 
 
-raw_data=pd.read_csv("C:/Users/aminv/Desktop/GSPC.csv")
+
+raw_data=pd.read_csv("C:/Users/aminv/Desktop/S&P500Data5Y.csv")
 
 #date from old to new
 daily_price_data = raw_data[['Date','Close']]
+
+#for plotting
+colors=['k','c','m','r','g','b']
 
 
 def build_series(data, data_points_per_series):
@@ -51,16 +59,38 @@ def fit(data, degree_for_fitting):
         fit_parameters.loc[fit_parameters.shape[0]] = row
     return fit_parameters
 
-def plot_fits(data):
+def plot_fits(data, color='k'):
     for index, row in data.iterrows():
         coefficients=row[1:]
         curve = poly.Polynomial(coefficients[::-1])
         x = np.linspace(0, 5, 256, endpoint=True)
         curves=plt.figure("curves")
-        curves = plt.plot(x, curve(x))
+        curves = plt.plot(x, curve(x), color=color)
     return curves
 
-def plot_4d(data):
+def fit_plane(df):
+    # do fit
+    tmp_A = []
+    tmp_b = []
+    for i in range(len(df['0'])):
+        tmp_A.append([df['0'][i], df['1'][i], 1])
+        tmp_b.append(df['2'][i])
+    b = np.matrix(tmp_b).T
+    A = np.matrix(tmp_A)
+    fit = (A.T * A).I * A.T * b
+    errors = b - A * fit
+    residual = np.linalg.norm(errors)
+    
+    print "solution:"
+    print "%f x + %f y + %f = z" % (fit[0], fit[1], fit[2])
+    print "errors:"
+    print errors
+    print "residual:"
+    print residual
+    return fit
+
+
+def plot_4d(data, plane=False):
     fig = plt.figure("4d_parameters")
     ax = fig.add_subplot(111, projection='3d')
     
@@ -70,14 +100,64 @@ def plot_4d(data):
     c = data['3']
 
     ax.scatter(x, y, z, c=c, cmap=plt.hot())
+    
+    if(plane):
+        # plot plane
+        fit=fit_plane(data)
+        xlim = ax.get_xlim()
+        ylim = ax.get_ylim()
+        X,Y = np.meshgrid(np.arange(xlim[0], xlim[1]),
+                          np.arange(ylim[0], ylim[1]))
+        Z = np.zeros(X.shape)
+        for r in range(X.shape[0]):
+            for c in range(X.shape[1]):
+                Z[r,c] = fit[0] * X[r,c] + fit[1] * Y[r,c] + fit[2]
+        ax.plot_wireframe(X,Y,Z, color='k')
+    
     plt.show()
-        
+
+
+
+    
         
         
 price_series_data=build_series(daily_price_data, data_points_per_series)
 fit_parameters=fit(price_series_data, degree_for_fitting)
 plot_fits(fit_parameters)
 if(degree_for_fitting==3):
-    plot_4d(fit_parameters)
+    plot_4d(fit_parameters, plane=True)
+    
+    
+def k_means_clustering(df):
+    kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit(df)
+    
+    labels = kmeans.predict(df)
+    centroids = kmeans.cluster_centers_
+    
+    df['Labels'] = labels
+    
+    if(degree_for_fitting==3):
+        fig = plt.figure("k_means_clustering")
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(df['0'], df['1'], df['2'], c=df['Labels'], cmap=plt.cm.tab10, alpha=0.5, edgecolor='k')
+        for centroid in centroids:
+            ax.scatter(centroid[0],centroid[1],centroid[2],c='black')
+        plt.show()
+    return df
 
+data_for_clustering=fit_parameters.loc[:, fit_parameters.columns != 'StartDate']
+data_for_clustering=data_for_clustering.loc[:, data_for_clustering.columns != '3']
 
+df=k_means_clustering(data_for_clustering)
+
+Cluster_curves=plt.figure("Cluster_curves")
+clusters=[]
+for i in range(n_clusters):
+    clusters=clusters+[df.loc[df['Labels'] == i]]
+    for index, row in clusters[i].iterrows():
+        coefficients=row[:-1]
+        curve = poly.Polynomial(coefficients[::-1])
+        x = np.linspace(0, 5, 256, endpoint=True)
+        Cluster_curves = plt.plot(x, curve(x), color=colors[i])
+plt.show()
+    
